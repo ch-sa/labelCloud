e8d7959d593ece0bd9fca00204f208748688790f
ch-sa <christoph.sager@tu-dresden.de>
2021-07-22 20:34:07 +0200

    Change clipping space and add to config.ini
     - increase OpenGL clipping space (0.1 - 300)
     - add near and far plane to config
     - add ignore rules to flake

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -9,9 +9,10 @@ def pytest_configure(config):
     print(f"Set working directory to {os.getcwd()}.")
 
     sys.path.insert(0, "labelCloud")
-    print(f"Added labelCloud to Python path.")
+    print("Added labelCloud to Python path.")
 
-    import app  # preventing circular import
+    # preventing circular import
+    import app  # noqa: E401
 
 
 @pytest.fixture
